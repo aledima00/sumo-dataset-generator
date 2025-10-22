b522b992f576d2d315e6fc22083d4e6eae6ad14f
Alessandro Di Matteo <aledimatteo00@gmail.com>
2025-10-22 17:09:24 +0200

    added edgeid to vehicles

--- sumodetector/vehicle.py
+++ sumodetector/vehicle.py
@@ -4,6 +4,7 @@ import traci as _traci
 @_dc
 class Vehicle:
     id:str = None
+    edge_id:str = None
     lane_id:str = None
     speed:float = 0.0
     acceleration:float = 0.0
@@ -11,9 +12,12 @@ class Vehicle:
 
     @classmethod
     def from_traci(cls, vid:str):
+        lid = _traci.vehicle.getLaneID(vid)
+        eid = _traci.lane.getEdgeID(lid)
         return cls(
             id=vid,
-            lane_id=_traci.vehicle.getLaneID(vid),
+            lane_id=lid,
+            edge_id=eid,
             speed=_traci.vehicle.getSpeed(vid),
             acceleration=_traci.vehicle.getAcceleration(vid)
         )
