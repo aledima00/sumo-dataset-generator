fcfc408a92bb61b95855dc9c9f51a6fa0d93d52c
Alessandro Di Matteo <aledimatteo00@gmail.com>
2025-10-25 22:12:08 +0200

    removed pack and vehicle as SPA no longer used

--- sumodetector/__init__.py
+++ sumodetector/__init__.py
@@ -1,6 +1,4 @@
 from .tracictl import runSimulation, TraciController, CollisionAction
 from .labels import LabelsEnum, MultiLabel
-from .pack import Frame, StaticPackAnalyzer
-from .vehicle import Vehicle
 
-__all__ = ['runSimulation', 'TraciController', 'CollisionAction', 'LabelsEnum', 'MultiLabel', 'Frame', 'StaticPackAnalyzer', 'Vehicle']
+__all__ = ['runSimulation', 'TraciController', 'CollisionAction', 'LabelsEnum', 'MultiLabel']
