6b0275da414f9eb511167a7c2fdd7045367bd586
Alessandro Di Matteo <aledimatteo00@gmail.com>
2025-10-26 16:38:02 +0100

    added LCP and JP to IP

--- genrouter/vehicles.py
+++ genrouter/vehicles.py
@@ -2,23 +2,46 @@ from dataclasses import dataclass as _dc, field as _field
 
 @_dc
 class IParams:
+    """
+    Generic parameters for the individual driver behavior.
+    - speedFactor: multiplier for the speed of the vehicle
+    - speedDev: deviation for the speed of the vehicle
+    - minGap: minimum gap to the leading vehicle in meters
+    Other parameters are related to lane changing and junction behavior.
+    """
     speedFactor:float = 1.0
     speedDev:float = 0.1
     minGap:float = 2.5
 
     # lane change parameters - keep default for majority of cases
-    lcAssertive:float=0.6
-    lcSpeedGain:float=0.5
-    lcStrategic:float=1.0
-    lcLookaheadLeft:float=2.0
-    lcLookaheadRight:float=1.0
-    lcKeepRight:float=0.7
-    lcCooperative:float=0.3
-    lcMinGap:float=2.5
-    lcMinGapLat:float=0.5
-    lcMinGapSpeed:float=5.0
-    lcMaxSpeedDeviation:float=5.0
-    lcReactionTime:float=0.5
+    lcStrategic:float=2.0 # how much to exploit lc to reach destinations
+    lcCooperative:float=0.5 # how much to perform lc in a cooperative manner
+    lcSpeedGain:float=2.0 # how much to exploit lc for speed gain
+    lcKeepRight:float=0.7 # how much [0-inf) to return right by default
+    lcContRight:float=0.85 # how much [0-1] to choose rightmost lane when available
+    lcOvertakeRight:float=0.01 # probability [0-1] to overtake on the right
+    lcOpposite:float=0.2 # how much [0-inf) to exploit opposite lane while overtaking
+    #lcStrategicLookahead:float=3000.0
+    #lcLookaheadLeft:float=2.0
+    #lcSpeedGainRight:float=0.1
+    lcSpeedGainLookahead:float=4.0 # time in seconds to anticipate slowdowns 
+    lcSpeedGainRemainTime:float=20.0 # after strategic lc, how much time to return on lane if no longer needed
+    #lcSpeedGainUrgency:float=50.0
+    lcAssertive:float=1.0 # willingness [0-inf) to acccept smaller gaps when changing lane
+    lcSigma:float=0.5 # imperfection in lateral positioning
+
+    # junction parameters - keep default for majority of cases
+    jmIgnoreKeepClearTime:float=10.0 # time in seconds after which keep-clear-junctions are ignored
+    jmIgnoreFoeProb:float=0.2 # probability [0-1] to ignore conflicting traffic below jmIgnoreFoeSpeed
+    jmIgnoreFoeSpeed:float=10/3.6 # speed (m/s) below which conflicting traffic is ignored
+    jmIgnoreJunctionFoeProb:float=0.05 # probability [0-1] to ignore conflicting traffic at junctions
+    jmStoplineGap:float=0.3 # gap to stopline in meters
+    jmStoplineCrossingGap:float=1.0 # gap to stopline when crossing in meters
+    jmStopSignWait:float=3.0 # time in seconds to wait at stop signs
+    jmAllwayStopWait:float=3.0 # time in seconds to wait at all-way stops
+    impatience:float=0.4 # how much [0-1] to get impatient and impede priority vehicles in traffic jams
+
+
 
 @_dc
 class VParams:
@@ -61,12 +84,17 @@ class VType:
         return self.id == other.id
     
 class Vehicle:
-    def __init__(self,id:str, vtype_id:str, route_id:str, depart_time:float):
+    def __init__(self,id:str, vtype_id:str, route_id:str, depart_time:float,*,additional_attributes:dict=None):
         self.id = id
         self.vtype_id = vtype_id
         self.route_id = route_id
         self.depart_time = depart_time
+        self.additional_attributes = additional_attributes if additional_attributes is not None else {}
     def xml(self):
-        return f'<vehicle id="{self.id}" type="{self.vtype_id}" depart="{self.depart_time}" route="{self.route_id}"></vehicle>'
-    
+        x = f'<vehicle id="{self.id}" type="{self.vtype_id}" depart="{self.depart_time}" route="{self.route_id}"'
+        for k,v in self.additional_attributes.items():
+            x += f' {k}="{v}"'
+        x += '/>'
+        return x
+
 __all__ = ["IParams", "VParams", "VType", "Vehicle"]
