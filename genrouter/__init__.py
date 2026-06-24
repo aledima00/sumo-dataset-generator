12796e2cbf6938887a970cf1d58df9cf7a14f7dd
Alessandro Di Matteo <aledimatteo00@gmail.com>
2026-06-24 17:52:56 +0200

    more consistent console

--- genrouter/__init__.py
+++ genrouter/__init__.py
@@ -1,11 +1,11 @@
 from .graph import GraphRepresentation, RouteRepresentation, ConnectionRepresentation, JunctionRepresentation
 from .vehicles import VParams, IParams, VType, Vehicle
 from .generator import Generator
-from .console import generate
+from .controller import GenerationController
 from .sumocfg import SumoCfg
 
 import colorama as _colorama
 _colorama.init(autoreset=True)
 
 
-__all__ = ['GraphRepresentation', 'RouteRepresentation', 'ConnectionRepresentation', 'JunctionRepresentation', 'IParams', 'VParams', 'VType', 'Vehicle', 'Generator', 'loadPyConfig', 'generate', 'SumoCfg']
+__all__ = ['GraphRepresentation', 'RouteRepresentation', 'ConnectionRepresentation', 'JunctionRepresentation', 'IParams', 'VParams', 'VType', 'Vehicle', 'Generator', 'loadPyConfig', 'GenerationController', 'SumoCfg']
